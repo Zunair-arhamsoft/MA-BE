c0e16f07ca2847c32fd145a89497f379aad38226
Zunair-arhamsoft <zunair.saeed@arhamsoft.com>
2025-11-10 17:35:13 +0500

    added headers for cors

--- router.js
+++ router.js
@@ -11,11 +11,8 @@ app.use(express.json());
 // CORS middleware
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Content-Type, Authorization, Cache-Control, X-Requested-With'
-  );
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
 
   if (req.method === 'OPTIONS') {
     return res.sendStatus(204);
